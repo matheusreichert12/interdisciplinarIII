10e7b263657741d8fd2867f94290d9261f353b7a
Matheus Vinicius Reichert <matheus.reichert@abase.local>
2019-09-03 17:46:28 -0300

    tela de criar conta

---
+++ interdisciplinar/lib/criarconta.page.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:interdisciplinar/login.page.dart';
+
+class CriarContaPage extends StatefulWidget {
+  @override
+  _CriarContaPageState createState() => _CriarContaPageState();
+}
+
+class _CriarContaPageState extends State<CriarContaPage> {
+  final _login = TextEditingController();
+  final _senha = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 100,
+              height: 100,
+              child: Image.asset("assets/tmj.png"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              controller: _login,
+              decoration: InputDecoration(
+                labelText: "Login",
+                labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20),
+              ),
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              controller: _senha,
+              decoration: InputDecoration(
+                labelText: "Senha",
+                labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20),
+              ),
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Text(
+                    "Salvar",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_login.text != null &&
+                        _login.text != "" &&
+                        _senha.text != null &&
+                        _senha.text != "") {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            content: new Text("Usuário salvo com sucesso"),
+                            actions: <Widget>[
+                              // define os botões na base do dialogo
+                              new FlatButton(
+                                child: new Text("Fechar"),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                      salvarConta();
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            content: new Text(
+                              "Preencha todos os campos!",
+                              style: new TextStyle(color: Colors.red),
+                            ),
+                            actions: <Widget>[
+                              // define os botões na base do dialogo
+                              new FlatButton(
+                                child: new Text("Fechar"),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 40,
+              child: FlatButton(
+                child: Text(
+                  "Ja possuí uma conta? Fazer login",
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: () => {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginPage())),
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void salvarConta() {
+    Firestore.instance.collection("usuarios").document().setData({
+      'login': _login.text,
+      'senha': _senha.text,
+      'admin': 0,
+      'nome': '',
+      'telefone': ''
+    });
+  }
+}
---
+++ interdisciplinar/lib/login.page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:interdisciplinar/criarconta.page.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _login = TextEditingController();
+  final _senha = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 100,
+              height: 100,
+              child: Image.asset("assets/tmj.png"),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              controller: _login,
+              decoration: InputDecoration(
+                labelText: "Login",
+                labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20),
+              ),
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              controller: _senha,
+              decoration: InputDecoration(
+                labelText: "Senha",
+                labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20),
+              ),
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 40,
+              child: FlatButton(
+                child: Text(
+                  "Cadastre-se",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () => {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CriarContaPage())),
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
